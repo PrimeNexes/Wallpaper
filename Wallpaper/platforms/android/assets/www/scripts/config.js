805c7883da73fd8c413bb4567726f42c33cea036
PrimeNexes <pranayxdesai@gmail.com>
2017-01-05 16:15:08 +0530

    0.19

---
+++ Wallpaper/platforms/android/assets/www/scripts/config.js
@@ -0,0 +1,8 @@
+﻿var config = {
+    apiKey: "AIzaSyDMA4hc2fAH7kQgjIxuBqVLGcgN4nvRP6I",
+    authDomain: "onsenfb-fba00.firebaseapp.com",
+    databaseURL: "https://onsenfb-fba00.firebaseio.com",
+    storageBucket: "onsenfb-fba00.appspot.com",
+    messagingSenderId: "695730046275"
+};
+firebase.initializeApp(config);
